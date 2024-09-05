cb001ed3f9fc6f23983d1ff7137875dc9b2d1439
CesarJimenezVilleda02 <A01703517@tec.mx>
2024-09-05 00:29:31 -0400

    Add getArea to shape interface in ts

--- typescript/src/renderer.ts
+++ typescript/src/renderer.ts
@@ -1,12 +1,12 @@
-import { newRectangle, Rectangle } from "./shapes/rectangle.js";
+import { Shape } from './shapes/shape.js'
 
-function newRenderer(rectangle: Rectangle) {
-    return {
-        draw() {
-            const area: number = rectangle.computeArea()
-            console.log("Shape drawn\n" + "Its area is " + area)
-        }
+function newRenderer(shapeToRender: Shape) {
+  return {
+    draw() {
+      const area: number = shapeToRender.computeArea()
+      console.log('Shape drawn\n' + 'Its area is ' + area)
     }
+  }
 }
 
 export { newRenderer }
